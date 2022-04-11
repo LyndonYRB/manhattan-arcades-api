d88b71aa5a914e22e3196287b8ad6eb8a4124640
Lyndon <lyndon.stluce@gmail.com>
2022-04-11 15:26:21 -0400

    still trying lol

--- Model/arcades.js
+++ Model/arcades.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-// let Arcade = new Schema({
-//   name: { type: String },
-//   address: { type: String },
-//   hours: { type: String },
-//   coverCharge: { type: Boolean },
-//   servesAlcohol: { type: Boolean },
-// });
+let Arcade = new Schema({
+  name: { type: String },
+  address: { type: String },
+  hours: { type: String },
+  coverCharge: { type: Boolean },
+  servesAlcohol: { type: Boolean },
+});
 
 export default mongoose.model("arcade", Arcade);
